0dc842f27b87705801d71522265c0c16083e5bf2
AshLaw96 <ash.l.96@outlook.com>
2025-03-13 17:11:13 +0000

    Add CommentList and CommentDetail views

---
+++ comments/views.py
@@ -0,0 +1,25 @@
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Comment
+from .serializer import CommentSerializer, CommentDetailSerializer
+
+
+class CommentList(generics.ListCreateAPIView):
+    """
+    List all comments or create a new comment
+    """
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update or delete a comment
+    """
+    queryset = Comment.objects.all()
+    serializer_class = CommentDetailSerializer
+    permission_classes = [IsOwnerOrReadOnly]
