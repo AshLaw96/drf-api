9b272d35ee081b69a4d1f92785baf7c4fe40fb49
AshLaw96 <ash.l.96@outlook.com>
2025-03-14 13:49:18 +0000

    Add all required packages & update files so can link Heroku

--- env.py
+++ env.py
@@ -3,7 +3,13 @@ import os
 os.environ['CLOUDINARY_URL'] = (
     'cloudinary://993648355866378:4icpKd8oMs6YNROoyOfTU5CwZ6Q@djudchnsf'
 )
-os.environ['SECRET_KEY'] = (
-    'django-insecure-kc#^(kz0ic=y(6!##hv2c%@u0r0ul6a@kz#jdi%p0t(o#9x7sd'
+os.environ.setdefault(
+    'SECRET_KEY',
+    'django-insecure-kc#^(kt2id=y(6!*#hc2c%@o9r0ul6a@lz/jdi%p0t(o#0x5sp'
 )
 os.environ['DEV'] = '1'
+os.environ['DATABASE_URL'] = (
+    'postgresql://neondb_owner:tDAQs67HIjiX@ep-empty-lake-a2gui66e'
+    '.eu-central-1.'
+    'aws.neon.tech/ditch_stoop_turf_475599'
+)
